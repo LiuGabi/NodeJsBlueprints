bc529d6499ac008079dd91a08cf4f39b8e39813c
Gabi <gabi@eyebuydirect.com>
2014-11-11 11:43:39 +0800

    ok

--- lesson1/cars.js
+++ lesson1/cars.js
@@ -1,6 +1,13 @@
 var wheels = require("./wheels.js");
 var Control = require("./control.js");
 
+var AirConditioning = require("./air.js");
+var air = new AirConditioning();
+air.on("started",function(data) {
+	console.log("Status: "+data.status);
+});
+air.start();
+
 wheels.init("winter");
 wheels.info();
 
